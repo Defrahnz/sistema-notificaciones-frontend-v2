fd2a36c68b9b59bb8668a09d74dbf23d237d8a0c
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-23 22:43:53 -0600

    Se agregó un diseño al componente de login y su funcionamiento al .ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm=this.formBuilder.group({
+    email:['dvaldivia98@hotmail.com',[Validators.required,Validators.email]],
+    password:['',Validators.required]
+  })
+  constructor(private formBuilder:FormBuilder, private toastr:ToastrService, private router:Router){}
+  ngOnInit(): void{}
 
+  get email(){
+    return this.loginForm.controls.email;
+  }
+
+  get password(){
+    return this.loginForm.controls.password;
+  }
+
+  login(){
+    if(this.loginForm.valid){
+      console.log("Se ha llamado al servicio de Login") 
+      this.toastr.success('Te has logeado correctamente','Éxito',{
+        timeOut:1000,
+      });
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    }else{
+      this.toastr.error( 'Ha habido un error al ingresar los datos','Error',{
+        timeOut:1000,
+      })
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
