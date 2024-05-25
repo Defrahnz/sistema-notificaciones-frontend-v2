51eecdd874356898176425cbceed6c20201973e9
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-25 16:19:37 -0600

    Creamos una función observable para trabajar con los datos

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from './usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  login(credentials:LoginRequest){
-    console.log(credentials);
+  login(credentials:LoginRequest): Observable<Usuario>{
+    return this.http.get<Usuario>('././assets/data.json')
   }
 }
