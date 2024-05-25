391f7c079f7e06e19344dd6a01150fb9a5592f82
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-25 16:40:31 -0600

    Agregamos un error Handler

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { Usuario } from './usuario';
 
 @Injectable({
@@ -12,6 +12,18 @@ export class LoginService {
   constructor(private http:HttpClient) { }
 
   login(credentials:LoginRequest): Observable<Usuario>{
-    return this.http.get<Usuario>('././assets/data.json')
+    return this.http.get<Usuario>('././assets/data.json').pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  private handleError(error:HttpErrorResponse){
+    if(error.status==0){
+      console.error('Se ha producido un error ', error.error);
+    }else{
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+
+    return throwError(()=> new Error('Algo fallo. Intenta nuevamente'))
   }
 }
