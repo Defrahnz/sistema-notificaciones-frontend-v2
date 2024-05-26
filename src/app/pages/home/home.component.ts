3a8739f52bbdeb8566294522feabf6ce79a087db
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-25 19:37:39 -0600

    Agregamos las funcionalidades al home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { Usuario } from 'src/app/services/auth/usuario';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   userLoginOn:boolean=false;
+  usuarioData?:Usuario;
+  constructor(private loginService:LoginService){}
+  ngOnInit(): void {
+    this.loginService.currentUsuarioLoginOn.subscribe({
+      next:(usuarioLoginOn)=>{
+        this.userLoginOn=usuarioLoginOn;
+      }
+    });
+
+    this.loginService.currentUsuarioData.subscribe({
+      next:(usuarioData)=>{
+        this.usuarioData=usuarioData;
+      }
+    })
+  }
+  
 
 }
