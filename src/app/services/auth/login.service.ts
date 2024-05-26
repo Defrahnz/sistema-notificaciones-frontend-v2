3c816c48e917f4554e9f555c56c551aa9370df89
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-25 19:20:25 -0600

    Mejoramos la vista de Login

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
 import { Usuario } from './usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  currentUsuarioLoginOn:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(false);
+  currentUsuarioData: BehaviorSubject<Usuario>=new BehaviorSubject<Usuario>({id:0,email:''});
 
   constructor(private http:HttpClient) { }
 
   login(credentials:LoginRequest): Observable<Usuario>{
     return this.http.get<Usuario>('././assets/data.json').pipe(
+      tap(usuarioData => {
+        this.currentUsuarioData.next(usuarioData);
+        this.currentUsuarioLoginOn.next(true);
+      }),
       catchError(this.handleError)
     )
   }
@@ -25,5 +31,13 @@ export class LoginService {
     }
 
     return throwError(()=> new Error('Algo fallo. Intenta nuevamente'))
+  }
+
+  get usuarioData():Observable<Usuario>{
+    return this.currentUsuarioData.asObservable();
+  }
+
+  get usuarioLoginOn():Observable<boolean>{
+    return this.currentUsuarioLoginOn.asObservable();
   }
 }
