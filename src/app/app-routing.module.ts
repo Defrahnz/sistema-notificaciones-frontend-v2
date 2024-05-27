f4b03520dcfb5f74e4c30f56b9ecb5a49aff9f4a
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-05-26 21:01:24 -0600

    Se agregó una ruta comodín para las url no validas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'inicio', pathMatch:'full'},
   {path:'inicio',component:HomeComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'**',component:NotfoundComponent}
 ];
 
 @NgModule({
