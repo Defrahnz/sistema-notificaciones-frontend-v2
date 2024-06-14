37b1974841599fa2ccd2caf4a9c5b900436156e4
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-14 01:23:11 -0600

    Se creó una nueva ruta para redireccionar al Módulo para editar el usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'inicio', pathMatch:'full'},
   {path:'inicio',component:HomeComponent},
   {path:'login',component:LoginComponent},
+  {path:'datos-usuario',component:UsuarioComponent},
   {path:'**',component:NotfoundComponent}
 ];
 
