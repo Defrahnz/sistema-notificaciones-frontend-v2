737c9e09ee8dc4c07185ce919a1a9f4322027db4
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-14 20:07:58 -0600

    Modificamos este archivo para que pueda conectarse con el servicio de los módulos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/auth/login.service';
 import { Usuario } from 'src/app/services/auth/usuario';
+import { ModuloService } from 'src/app/services/modulo/modulo.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,17 @@ import { Usuario } from 'src/app/services/auth/usuario';
 export class HomeComponent implements OnInit{
   userLoginOn:boolean=false;
   usuarioData?:Usuario;
-  constructor(private loginService:LoginService){}
+  modulos:any[]=[];
+  constructor(private loginService:LoginService, private moduloService:ModuloService){}
   ngOnInit(): void {
+    this.moduloService.obtenerModulosActivos().subscribe(
+      (data:any)=>{
+        this.modulos=data;
+      },
+      error=>{
+        console.error("Ocurrió un error al obtener los módulos")
+      }
+    )
     this.loginService.currentUsuarioLoginOn.subscribe({
       next:(usuarioLoginOn)=>{
         this.userLoginOn=usuarioLoginOn;
