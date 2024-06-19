9a8f5543b3b2473f6d80458df00fc2739dc81cad
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-06-19 17:46:33 -0600

    Cambio requeridos para que se pueda realizar la conexión pertinente del back con el front

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,39 +1,49 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
-import { Usuario } from './usuario';
+import { BehaviorSubject, Observable, catchError, map, tap, throwError } from 'rxjs';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   currentUsuarioLoginOn:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(false);
-  currentUsuarioData: BehaviorSubject<Usuario>=new BehaviorSubject<Usuario>({id:0,username:''});
+  currentUsuarioData: BehaviorSubject<String>=new BehaviorSubject<String>("");
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.currentUsuarioLoginOn=new BehaviorSubject<boolean>(sessionStorage.getItem("token")!=null);
+    this.currentUsuarioData=new BehaviorSubject<String>(sessionStorage.getItem("token") || "");
+   }
 
-  login(credentials:LoginRequest): Observable<Usuario>{
-    return this.http.get<Usuario>('././assets/data.json').pipe(
-      tap(usuarioData => {
-        this.currentUsuarioData.next(usuarioData);
+  login(credentials:LoginRequest): Observable<any>{
+    return this.http.post<any>(environment.urlHost+"auth/login",credentials).pipe(
+      tap((usuarioData) => {
+        sessionStorage.setItem("token", usuarioData.token);
+        this.currentUsuarioData.next(usuarioData.token);
         this.currentUsuarioLoginOn.next(true);
       }),
+      map((usuarioData)=>usuarioData.token),
       catchError(this.handleError)
-    )
+    ) 
+  }
+
+  logout():void{
+    sessionStorage.removeItem("token");
+    this.currentUsuarioLoginOn.next(false);
   }
 
   private handleError(error:HttpErrorResponse){
     if(error.status==0){
       console.error('Se ha producido un error ', error.error);
     }else{
-      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+      console.error('Se retornó el código de estado ', error);
     }
 
     return throwError(()=> new Error('Algo fallo. Intenta nuevamente'))
   }
 
-  get usuarioData():Observable<Usuario>{
+  get usuarioData():Observable<String>{
     return this.currentUsuarioData.asObservable();
   }
 
