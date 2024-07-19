c4722dad0af6a7c80bf8996c6ffe649278b758a3
Daniel Valdivia <dvaldivia98@hotmail.com>
2024-07-19 01:44:01 -0600

    Quitamos los requeridos por que mostraba un error (fix later)

--- src/app/services/auth/usuario.ts
+++ src/app/services/auth/usuario.ts
@@ -1,17 +1,17 @@
 export interface Usuario{
     id:number;
     username:string;
-    nombre?:string;
-    apellidop?:string;
-    apellidom?:string;
-    calle?:string;
-    numinterior?:string;
-    numexterior?:string;
-    colonia?:string;
-    ciudad?:string;
-    codigopostal?:string;
-    telefonocasa?:string;
-    telefonomovil?:string;
+    nombre:string;
+    apellidop:string;
+    apellidom:string;
+    calle:string;
+    numinterior:string;
+    numexterior:string;
+    colonia:string;
+    ciudad:string;
+    codigopostal:string;
+    telefonocasa:string;
+    telefonomovil:string;
     message?: string;
 
 }
